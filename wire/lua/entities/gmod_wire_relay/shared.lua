79ddabf9b291fd7ac576f3a16965ae2e33e545df
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-02-05 06:34:54 +0000

    added second version of my relay. first was hard coded DPDT proof of concept and called switch. this one is 1-8 pole, 1-10 throw and uses less code.

---
+++ wire/lua/entities/gmod_wire_relay/shared.lua
@@ -0,0 +1,13 @@
+
+
+ENT.Type 			= "anim"
+ENT.Base 			= "base_wire_entity"
+
+ENT.PrintName		= "Wired Relay"
+ENT.Author			= "tad2020"
+ENT.Contact			= ""
+ENT.Purpose			= ""
+ENT.Instructions	= ""
+
+ENT.Spawnable			= false
+ENT.AdminSpawnable		= false
