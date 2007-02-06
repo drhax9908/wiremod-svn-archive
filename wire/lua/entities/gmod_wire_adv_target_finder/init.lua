c6196a0216c612eadfdffe2de18c0bd07a66a2ae
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-02-06 04:51:45 +0000

    added Shadow25's advanced target finder

--- wire/lua/entities/gmod_wire_adv_target_finder/init.lua
+++ wire/lua/entities/gmod_wire_adv_target_finder/init.lua
@@ -4,7 +4,7 @@ AddCSLuaFile( "shared.lua" )
 
 include('shared.lua')
 
-ENT.WireDebugName = "Targetter"
+ENT.WireDebugName = "AdvTargetter"
 ENT.OverlayDelay = 0
 
 local MODEL = Model( "models/props_lab/powerbox02d.mdl" )
@@ -15,23 +15,27 @@ function ENT:Initialize()
 	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
 	self.Entity:SetSolid( SOLID_VPHYSICS )
 
-	self.Inputs = Wire_CreateInputs(self.Entity, { "Hold" })
+	self.Inputs = Wire_CreateInputs(self.Entity, { "Hold", "Max Range", "Min Range" })
 	self.Outputs = Wire_CreateOutputs(self.Entity, { "Out" })
 end
 
 
-function ENT:Setup(range, players, npcs, beacons)
+function ENT:Setup(range, min_range, players, npcs, beacons, hoverballs, thrusters, rpgs)
 	self.Range = range
+	self.Minimum = min_range
 	self.TargetPlayer = players
 	self.TargetNPC = npcs
 	self.TargetBeacon = beacons
+	self.TargetHoverballs = hoverballs
+	self.TargetThrusters = thrusters
+	self.TargetRPGs = rpgs
 
 	self:ShowOutput(false)
 	Wire_TriggerOutput(self.Entity, "Out", 0)
 end
 
 
-function ENT:GetBeaconPos(sensor)
+function ENT:GetBeaconPos()
 	if (self.Target) then
 	    if (not self.Target:IsValid()) then
 	        self.Target = nil
@@ -57,7 +61,7 @@ function ENT:Think()
 		if (self.Target) and (self.Target:IsValid()) and (self.NextTargetTime) and (CurTime() < self.NextTargetTime) then return end
 		self.NextTargetTime = CurTime()+1
 
-		local targets = ents.FindInSphere(self.Entity:GetPos(), self.Range or 10)
+		local targets = ents.FindInSphere(self.Entity:GetPos(), self.Range)
 		local mypos = self.Entity:GetPos()
 
 		self.Target = nil
@@ -70,11 +74,19 @@ function ENT:Think()
 				tt = target
 			elseif (self.TargetBeacon) and (target:GetClass() == "gmod_wire_locator") then
 				tt = target
+			elseif (self.TargetRPGs) and (target:GetClass() == "rpg_missle") then
+				tt = target
+			elseif (self.TargetHoverballs) and (target:GetClass() == "gmod_hoverball" || target:GetClass() == "gmod_wire_hoverball") then
+				tt = target
+			elseif (self.TargetRPGs) and (target:GetClass() == "rpg_missile") then
+				tt = target
+			elseif (self.TargetThrusters) and (target:GetClass() == "gmod_thruster" || target:GetClass() == "gmod_wire_thruster") then
+				tt = target
 			end
-
+			
 			if (tt) then
 			    local dist = (tt:GetPos() - mypos):Length()
-			    if (dist < mindist) then
+			    if (dist < mindist && dist > self.Minimum) then
 			        mindist = dist
 			        self.Target = tt
 			    end
@@ -93,11 +105,11 @@ end
 
 
 function ENT:ShowOutput(value)
-	local txt = "Target Finder - "
+	local txt = "Adv Target Finder - "
 	if (value) then
-		txt = txt .. "Targetting"
+		txt = txt .. "Targetting, Range:" .. self.Minimum .. "-" .. self.Range
 	else
-		txt = txt .. "No Target"
+		txt = txt .. "No Target, Range:" .. self.Minimum .. "-" .. self.Range
 	end
 	
 	if (self.Inputs.Hold) and (self.Inputs.Hold.Value > 0) then
@@ -105,4 +117,16 @@ function ENT:ShowOutput(value)
 	end
 	
 	self:SetOverlayText(txt)
+end
+
+/*---------------------------------------------------------
+   Name: TriggerInput
+   Desc: the inputs
+---------------------------------------------------------*/
+function ENT:TriggerInput(iname, value)
+	if (iname == "Max Range") then
+		self.Range = value
+	elseif (iname == "Min Range") then
+	    self.Minimum = value
+	end
 end
