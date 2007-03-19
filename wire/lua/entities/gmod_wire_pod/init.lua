15befb3331f8787fba223bf59079bb11258ae89b
itsbth <itsbth@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-03-19 19:43:40 +0000

    Wired pod!

---
+++ wire/lua/entities/gmod_wire_pod/init.lua
@@ -0,0 +1,128 @@
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+ENT.WireDebugName = "Pod Controller"
+
+local MODEL = Model("models/jaanus/wiretool/wiretool_siren.mdl")
+
+-- Number of pods (Used for creating an uniqe name)
+-- wire_pod_count = 0
+
+function ENT:Initialize()
+	self.Entity:SetModel( MODEL )
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
+	self.Entity:SetSolid( SOLID_VPHYSICS )
+	
+	--[[
+	-- Set the name of self (To let us have several controllers)
+	-- Note: Player:GetVehicle does actually work on pods. (&/(%&/(/&%)(/( wiki)
+	self.Tname = "wpod"..wire_pod_count
+	wire_pod_count = wire_pod_count + 1
+	self.Entity:SetName(self.Tname)
+	
+	]]
+	-- Output keys. Format: keys["name"] = IN_*
+	keys = { }
+	keys["W"] = IN_FORWARD
+	keys["A"] = IN_MOVELEFT
+	keys["S"] = IN_BACK
+	keys["D"] = IN_MOVERIGHT
+	keys["Mouse1"] = IN_ATTACK
+	keys["R"] = IN_RELOAD
+	
+	-- Invert the table to use it with Wire_CreateOutputs
+	local outputs = { }
+	local n = 1
+	
+	for k, v in pairs( keys ) do
+		outputs[n] = k
+		n = n + 1
+	end
+	-- Create outputs
+	self.Outputs = Wire_CreateOutputs( self.Entity, outputs )
+end
+
+function ENT:OnRemove()
+	Wire_Remove(self.Entity)
+end
+
+-- Link to pod
+function ENT:Setup( pod )
+	-- print("Setting up... ".."\"PlayerOn "..self.Tname..",start,1,0,-1\"")
+	-- print_r(pod)
+	-- 
+	self.Pod = pod
+	self.TTLFP = CurTime()
+end
+
+-- No inputs
+function ENT:TriggerInput(iname, value)
+
+end
+
+function ENT:ShowOutput(value)
+	if (value ~= self.PrevOutput) then
+		self:SetOverlayText( "Pod" )
+		self.PrevOutput = value
+	end
+end
+
+function ENT:OnRestore()
+    Wire_Restored(self.Entity)
+end
+
+--[[
+-- Gets called when the SENT get an input (i.e. from the pod)
+function ENT:AcceptInput( name, activator, caller )
+	-- print("Name = "..tostring(name))
+	if not activator:IsPlayer() then return end
+	if name == "start" then
+		self.Cply = activator
+		-- print("Activator: "..activator:GetClass().." Caller: "..caller:GetClass())
+	elseif name == "stop" then
+		self.Cply = nil
+	end
+	return true
+end
+]]
+
+-- Called every 0.01 seconds, check for key down
+function ENT:Think()
+	-- Check that we have a pod
+	if self.Pod then
+		-- Check if we should look for player entering/exiting the vehicle TTLFP = TimeToLookForPod
+		if self.TTLFP < CurTime() then
+			-- Check if the old player is still in our vehicle
+			if !(self.Ply and self.Ply:GetVehicle() == self.Pod) then
+				-- Get all players
+				local plys = player.GetAll()
+				self.Ply = nil
+				-- Loop through all players and check if their vehicle is our vehicle
+				for k,v in pairs(plys) do
+					if v:GetVehicle() == self.Pod then self.Ply = v end
+				end
+			end
+			-- Look for players again in 1/10 second
+			self.TTLFP = CurTime() + 0.1
+		end
+		
+		if self.Ply then
+			-- Loop through all the keys, and check if they was pressed last frame
+			for k, v in pairs( keys )  do
+				--[[
+				if self.Ply:KeyDownLast( v ) then Wire_TriggerOutput( self.Entity, k, 1 )-- ; print( "Pressed: "..k..":"..v )
+				else Wire_TriggerOutput( self.Entity, k, 0 ) end
+				]]
+				-- Optimising Optimising Optimising...
+				--print("SPAM!")
+				if self.Ply:KeyDownLast( v ) then Wire_TriggerOutput( self.Entity, k, 1 )
+				else Wire_TriggerOutput( self.Entity, k, 0 ) end
+			end
+		end
+	end
+	self.Entity:NextThink(CurTime() + 0.01)
+	return true
+end
---
+++ wire/lua/weapons/gmod_tool/stools/wire_pod.lua
@@ -0,0 +1,162 @@
+
+TOOL.Category		= "Wire - Whatever"
+TOOL.Name			= "Pod"
+TOOL.Command		= nil
+TOOL.ConfigName		= ""
+
+if ( CLIENT ) then
+    language.Add( "Tool_wire_pod_name", "Pod Controller Tool (Wire)" )
+    language.Add( "Tool_wire_pod_desc", "Spawn/link a Wire Pod controller." )
+    language.Add( "Tool_wire_pod_0", "Primary: Create Pod controller. Secondary: Link controller." )
+    language.Add( "WirePodTool_pod", "Pod:" )
+	language.Add( "sboxlimit_wire_pods", "You've hit your Pod Controller limit!" )
+	language.Add( "undone_wirepod", "Undone Wire Pod Controller" )
+end
+
+if (SERVER) then
+	CreateConVar('sbox_maxwire_pods', 20)
+end
+
+TOOL.Model = "models/jaanus/wiretool/wiretool_siren.mdl"
+
+cleanup.Register( "wire_pods" )
+
+function TOOL:LeftClick( trace )
+	if (!trace.HitPos) then return false end
+	if (trace.Entity:IsPlayer()) then return false end
+	if ( CLIENT ) then return true end
+
+	local ply = self:GetOwner()
+
+	if ( trace.Entity:IsValid() && trace.Entity:GetClass() == "gmod_wire_pod" && trace.Entity:GetTable().pl == ply ) then
+		return true
+	end
+
+	if ( !self:GetSWEP():CheckLimit( "wire_pods" ) ) then return false end
+
+	local Ang = trace.HitNormal:Angle()
+	Ang.pitch = Ang.pitch + 90
+
+	local wire_pod = MakeWirePod( ply, trace.HitPos, Ang )
+
+	local min = wire_pod:OBBMins()
+	wire_pod:SetPos( trace.HitPos - trace.HitNormal * min.z )
+
+	local const, nocollide
+
+	// Don't weld to world
+	if ( trace.Entity:IsValid() ) then
+		const = constraint.Weld( wire_pod, trace.Entity, 0, trace.PhysicsBone, 0, true )
+		trace.Entity:DeleteOnRemove( wire_pod )
+		// Don't disable collision if it's not attached to anything
+		wire_pod:GetPhysicsObject():EnableCollisions( false )
+		wire_pod:GetTable().nocollide = true
+	end
+
+	undo.Create("Wire Pod")
+		undo.AddEntity( wire_pod )
+		undo.AddEntity( const )
+		undo.SetPlayer( ply )
+	undo.Finish()
+
+
+	ply:AddCleanup( "wire_pods", wire_pod )
+
+	return true
+end
+
+function TOOL:RightClick( trace )
+	if (!trace.HitPos) then return false end
+	--if trace.Entity:GetClass() != "gmod_wire_pod" || "prop_vehicle_prisoner_pod" then return false end
+	if ( CLIENT ) then return true end
+	if (self.Oldent) then
+		-- Don't ask...
+		if trace.Entity:GetClass() == "prop_vehicle_prisoner_pod" or true then self.Oldent:GetTable():Setup(trace.Entity); self.Oldent = nil; return true end
+	else
+		if trace.Entity:GetClass() == "gmod_wire_pod" then self.Oldent = trace.Entity; return true end
+	end
+	return false
+end
+
+function TOOL:Reload(trace)
+	self.Oldent = nil
+end
+
+if (SERVER) then
+
+	function MakeWirePod( pl, Pos, Ang, pod )
+		if ( !pl:CheckLimit( "wire_pods" ) ) then return false end
+	
+		local wire_pod = ents.Create( "gmod_wire_pod" )
+		if (!wire_pod:IsValid()) then return false end
+
+		wire_pod:SetAngles( Ang )
+		wire_pod:SetPos( Pos )
+		wire_pod:SetModel( Model("models/jaanus/wiretool/wiretool_siren.mdl") )
+		wire_pod:Spawn()
+		--Setup on secondary
+		--wire_pod:GetTable():Setup( )
+		if pod then wire_pod:GetTable():Setup( pod ) end
+		wire_pod:GetTable():SetPlayer( pl )
+
+		local ttable = {
+			pl = pl,
+		}
+
+		table.Merge(wire_pod:GetTable(), ttable )
+		
+		pl:AddCount( "wire_pods", wire_pod )
+
+		return wire_pod
+	end
+	
+	duplicator.RegisterEntityClass("gmod_wire_pod", MakeWirePod, "Pos", "Ang", "Pod", "Vel", "aVel", "frozen",)
+
+end
+
+function TOOL:UpdateGhostWirePod( ent, player )
+	if ( !ent || !ent:IsValid() ) then return end
+
+	local tr 	= utilx.GetPlayerTrace( player, player:GetCursorAimVector() )
+	local trace 	= util.TraceLine( tr )
+
+	if (!trace.Hit || trace.Entity:IsPlayer() || trace.Entity:GetClass() == "gmod_wire_pod" ) then
+		ent:SetNoDraw( true )
+		return
+	end
+
+	local Ang = trace.HitNormal:Angle()
+	Ang.pitch = Ang.pitch + 90
+
+	local min = ent:OBBMins()
+	ent:SetPos( trace.HitPos - trace.HitNormal * min.z )
+	ent:SetAngles( Ang )
+
+	ent:SetNoDraw( false )
+end
+
+function TOOL:Think()
+	if (!self.GhostEntity || !self.GhostEntity:IsValid() || self.GhostEntity:GetModel() != self.Model ) then
+		self:MakeGhostEntity( self.Model, Vector(0,0,0), Angle(0,0,0) )
+	end
+
+	self:UpdateGhostWirePod( self.GhostEntity, self:GetOwner() )
+end
+
+function TOOL.BuildCPanel(panel)
+	panel:AddControl("Header", { Text = "#Tool_wire_pod_name", Description = "#Tool_wire_pod_desc" })
+
+	panel:AddControl("ComboBox", {
+		Label = "#Presets",
+		MenuButton = "1",
+		Folder = "wire_pod",
+
+		Options = {
+			Default = {
+				wire_pod_pod = "0",
+			}
+		},
+		CVars = {
+		}
+	})
+end
