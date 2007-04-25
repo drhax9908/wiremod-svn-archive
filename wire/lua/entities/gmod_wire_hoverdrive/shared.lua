6ebcf37e5100cbee9594307be2be48256830d874
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-04-25 23:58:43 +0000

    adding alpha test of hover drives

---
+++ wire/lua/entities/gmod_wire_hoverdrive/shared.lua
@@ -0,0 +1,59 @@
+
+
+ENT.Type = "anim"
+ENT.Base = "base_wire_entity"
+
+ENT.PrintName		= "Hover Drive"
+ENT.Author			= "TAD2020"
+ENT.Contact			= ""
+ENT.Purpose			= ""
+ENT.Instructions	= ""
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= false
+
+function ENT:GetTargetZ()
+	return self.Entity:GetNetworkedInt( 0 )
+end
+function ENT:SetTargetZ( z )
+	return self.Entity:SetNetworkedInt( 0, z )
+end
+
+/*function ENT:GetTargetX()
+	return self.Entity:GetNetworkedInt( 1 )
+end
+function ENT:SetTargetX( x )
+	return self.Entity:SetNetworkedInt( 1, x )
+end
+
+function ENT:GetTargetY()
+	return self.Entity:GetNetworkedInt( 2 )
+end
+function ENT:SetTargetY( y )
+	return self.Entity:SetNetworkedInt( 2, y )
+end*/
+
+
+function ENT:GetSpeed()
+
+	// Sensible limits
+	if (!SinglePlayer()) then
+		return math.Clamp( self.Entity:GetNetworkedFloat( 0 ), 0.0, 10.0 )
+	end
+
+	return self.Entity:GetNetworkedFloat( 0 )
+end
+
+function ENT:SetSpeed( s )
+	
+	self.Entity:SetNetworkedFloat( 0, s )
+	
+end
+
+function ENT:GetHoverMode()
+	return self.Entity:GetNetworkedBool( 0 )
+end
+
+function ENT:SetHoverMode( h )
+	return self.Entity:SetNetworkedBool( 0, h )
+end
