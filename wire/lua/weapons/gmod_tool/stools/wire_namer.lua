f9f3e4dfbb68f67ce9ab10df26c4f9e11f869005
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-05-06 04:03:51 +0000

    made name duplicatable

--- wire/lua/weapons/gmod_tool/stools/wire_namer.lua
+++ wire/lua/weapons/gmod_tool/stools/wire_namer.lua
@@ -13,6 +13,14 @@ end
 
 TOOL.ClientConVar[ "name" ] = ""
 
+local function SetName( Player, Entity, Data )
+	if ( Data and Data.name ) then
+		Entity:SetNetworkedString("WireName", Data.name)
+		duplicator.StoreEntityModifier( Entity, "WireName", Data )
+	end
+end
+duplicator.RegisterEntityModifier( "WireName", SetName )
+
 function TOOL:LeftClick(trace)
 	if (not trace.Entity:IsValid()) then return end
 	if (CLIENT) then return end
@@ -20,7 +28,10 @@ function TOOL:LeftClick(trace)
 
 	local name = self:GetClientInfo("name")
 	
-	trace.Entity:SetNetworkedString("WireName", name)
+	//trace.Entity:SetNetworkedString("WireName", name)
+	
+	//made the WireName duplicatable entmod (TAD2020)
+	SetName( Player, trace.Entity, {name = name} )
 
 	return true
 end
