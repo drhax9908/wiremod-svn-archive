c602676abb0772fdc6203d8495b18d7480988c16
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-05-06 04:08:32 +0000

    move GPS pos to client side

--- wire/lua/entities/gmod_wire_gps/cl_init.lua
+++ wire/lua/entities/gmod_wire_gps/cl_init.lua
@@ -3,3 +3,15 @@ ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
 
 include('shared.lua')
+
+
+function ENT:Think()
+	self.BaseClass.Think(self)
+
+    local pos = self.Entity:GetPos()
+	local txt = "Position = " .. math.Round(pos.x*1000)/1000 .. "," .. math.Round(pos.y*1000)/1000 .. "," .. math.Round(pos.z*1000)/1000
+	self.Entity:SetNetworkedBeamString( "GModOverlayText", txt )
+	
+	self.Entity:NextThink(CurTime()+0.04)
+	return true
+end
