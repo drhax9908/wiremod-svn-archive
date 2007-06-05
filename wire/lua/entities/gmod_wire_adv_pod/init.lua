7ea72416ac092e68158ee28e6e0e23c322a0c28f
Tomb332 <Tomb332@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-06-05 18:29:26 +0000

    advanced pod updates.

--- wire/lua/entities/gmod_wire_adv_pod/init.lua
+++ wire/lua/entities/gmod_wire_adv_pod/init.lua
@@ -57,11 +57,24 @@ function ENT:Initialize()
 	-- </angle>
 	
 	n = n + 1
+	outputs[n] = "Distance"
+	--[[ elevation / bearing outputs
+	n = n + 1
+	outputs[n] = "Bearing"
+	
+	n = n + 1
+	outputs[n] = "Elevation"
+	--]]
+	n = n + 1
 	outputs[n] = "Active"
+	
+	n = n + 1
+	outputs[n] = "team"
 	
 	-- Create outputs
 	self.Outputs = Wire_CreateOutputs( self.Entity, outputs )
-	self:SetOverlayText( "Pod Controller" )
+	self.Inputs = Wire_CreateInputs( self.Entity, { "Lock", "Terminate", "Strip weapons", "Eject" } )
+	self:SetOverlayText( "Adv. Pod Controller" )
 end
 
 -- Link to pod
@@ -70,9 +83,34 @@ function ENT:Setup( pod )
 	self.TTLFP = CurTime()
 end
 
--- No inputs
 function ENT:TriggerInput(iname, value)
-
+		if (iname == "Lock") then
+			if !(self.Pod && self.Pod:IsValid()) then return end
+				if value > 0 then
+					self.Pod:Fire("Lock", "1", 0)
+				else
+					self.Pod:Fire("Unlock", "1", 0)
+				end
+	elseif (iname == "Terminate") then
+		if self.Ply then
+			if (value >= 1) then
+				self.Ply:Kill()
+			end
+		end
+	elseif (iname == "Strip weapons") then
+		if self.Ply then
+			if (value >= 1) then
+				self.Ply:StripWeapons( )
+				self.Ply:PrintMessage(HUD_PRINTTALK,"Your weapons have been stripped!\n")
+			end
+		end
+	elseif (iname == "Eject") then
+		if self.Ply then
+			if (value >= 1) then
+				self.Ply:ExitVehicle( )
+			end
+		end
+	end
 end
 
 function ENT:ShowOutput(value)
@@ -104,6 +142,7 @@ end
 
 -- Called every 0.01 seconds, check for key down
 function ENT:Think()
+local brng = Angle(0, 0, 0)
 	-- Check that we have a pod
 	if self.Pod then
 		-- Check if we should look for player entering/exiting the vehicle TTLFP = TimeToLookForPod
@@ -119,8 +158,11 @@ function ENT:Think()
 					if v:GetVehicle() == self.Pod then self.Ply = v end
 				end
 				
-				if self.Ply then Wire_TriggerOutput( self.Entity, "Active", 1)
-				else Wire_TriggerOutput( self.Entity, "Active", 0) end
+				if self.Ply then
+				Wire_TriggerOutput( self.Entity, "Active", 1)
+				else
+				Wire_TriggerOutput( self.Entity, "Active", 0) 
+				end
 			end
 			-- Look for players again in 1/10 second
 			self.TTLFP = CurTime() + 0.1
@@ -140,6 +182,29 @@ function ENT:Think()
 				Wire_TriggerOutput( self.Entity, "X", tmp.x )
 				Wire_TriggerOutput( self.Entity, "Y", tmp.y )
 				Wire_TriggerOutput( self.Entity, "Z", tmp.z )
+				local MyPos = self.Entity:GetPos()
+				dist = (tmp-MyPos):Length()
+				Wire_TriggerOutput( self.Entity, "Distance", dist)
+				--[[ Non-working elevation / bearing code
+				local DeltaPos = self.Entity:WorldToLocal(tmp)
+				brng = DeltaPos:Angle( )
+				local pitch = brng.p
+				local yaw = brng.y
+				if (yaw > 180) then yaw = yaw - 360 end
+				if (yaw < -180) then yaw = yaw + 360 end
+				if (yaw > 180) then yaw = yaw - 360 end
+				if (yaw < -180) then yaw = yaw + 360 end
+				if (pitch > 180) then pitch = ypitch - 360 end
+				if (pitch < -180) then pitch = pitch + 360 end
+				if (pitch > 180) then pitch = pitch - 360 end
+				if (pitch < -180) then pitch = pitch + 360 end
+				Wire_TriggerOutput(self.Entity, "Bearing", -yaw - 90)
+				Wire_TriggerOutput(self.Entity, "Elevation", -pitch)
+				--]]
+				local plyteam = self.Ply:Team( )
+				Wire_TriggerOutput( self.Entity, "Team", plyteam)
+				
+				
 			end
 		end
 	end
