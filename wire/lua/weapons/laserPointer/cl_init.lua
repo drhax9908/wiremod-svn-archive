81b7dc1db159034113712e76cde1d6bdc7c8146e
itsbth <itsbth@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-06-12 12:49:29 +0000

    :(

--- wire/lua/weapons/laserPointer/cl_init.lua
+++ wire/lua/weapons/laserPointer/cl_init.lua
@@ -5,18 +5,30 @@ SWEP.SlotPos = 4
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = true
 
+local LASER = Material('cable/redlaser')
+
+function SWEP:Initialize()
+    local ply = LocalPlayer()
+    self.VM = ply:GetViewModel()
+    local attachmentIndex = self.VM:LookupAttachment("muzzle")
+    if attachmentIndex == 0 then attachmentIndex = self.VM:LookupAttachment("1") end
+	self.Attach = attachmentIndex
+end
+
 function SWEP:ViewModelDrawn()
-    local pos = self.Owner:GetShootPos()
-    local ang = self.Owner:GetAimVector()
-    local tracedata = {}
-    tracedata.start = pos
-    tracedata.endpos = pos + ang * 100000
-    tracedata.filter = self.Owner
-    local trace = util.TraceLine(tracedata)
-    if(self.pointing) then
+	if(self.Weapon:GetNWBool("Active")) then
+	--[[
+	    local pos = self.Owner:GetShootPos()
+	    local tracedata = {}
+			tracedata.start = pos
+			tracedata.endpos = pos + self.Owner:GetAimVector() * 100000
+			tracedata.filter = self.Owner
+	    local trace = util.TraceLine(tracedata)
+		]]--
         //Draw the laser beam.
-        render.SetMaterial(Material("tripmine_laser"))
-	    render.DrawBeam(trace.StartPos, trace.HitPos, 6, 0, 10, Color(255,0,0,255))
+        render.SetMaterial( LASER )
+	    //render.DrawBeam(trace.StartPos, trace.HitPos, 6, 0, 10, Color(255,0,0,255))
+		render.DrawBeam(self.VM:GetAttachment(self.Attach).Pos, self.Owner:GetEyeTrace().HitPos, 2, 0, 12.5, Color(255, 0, 0, 255))
         //Msg("Laser\n")
     end
 end
--- wire/lua/weapons/laserPointer/init.lua
+++ wire/lua/weapons/laserPointer/init.lua
@@ -5,3 +5,54 @@ include('shared.lua')
 SWEP.Weight = 8
 SWEP.AutoSwitchTo = false
 SWEP.AutoSwitchFrom = false
+
+SWEP.Receiver = nil
+SWEP.Pointing = false
+
+function SWEP:Initialize()
+    self.Pointing = false
+end
+
+function SWEP:Reload()
+	
+end
+
+function SWEP:PrimaryAttack()
+    Msg("Fire\n")
+	self.Pointing = !self.Pointing
+	Msg("self.Pointing = " .. tostring(self.Pointing) .. "\n")
+	self.Weapon:SetNWBool("Active", self.Pointing)
+end
+
+function SWEP:SecondaryAttack()
+	Msg("Secondary\n")
+    local pos = self.Owner:GetShootPos()
+    local tracedata = {}
+	    tracedata.start = pos
+	    tracedata.endpos = pos + self.Owner:GetAimVector() * 100000
+	    tracedata.filter = self.Owner
+    local trace = util.TraceLine(tracedata)
+    
+    if (trace.Entity:IsValid() && trace.Entity:GetClass() == "gmod_wire_las_Receiver") then
+        self.Receiver = trace.Entity
+        return true
+    end
+end
+
+function SWEP:Think()
+    if(self.Pointing && self.Receiver && self.Receiver:IsValid())then
+        local pos = self.Owner:GetShootPos()
+        local tracedata = {}
+	        tracedata.start = pos
+	        tracedata.endpos = pos + self.Owner:GetAimVector() * 100000
+	        tracedata.filter = self.Owner
+        local trace = util.TraceLine(tracedata)
+    
+        local point = trace.HitPos
+        Wire_TriggerOutput(self.Receiver, "X", point.x)
+        Wire_TriggerOutput(self.Receiver, "Y", point.y)
+        Wire_TriggerOutput(self.Receiver, "Z", point.z)
+        Wire_TriggerOutput(self.Receiver, "Dist", self.Receiver:GetPos():Distance(point))
+        //Msg("Send!\n")
+    end
+end
