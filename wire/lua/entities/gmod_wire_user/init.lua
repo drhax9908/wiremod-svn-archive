91c38682bb935b38e47fad1516267beefdd0a9aa
Tomb332 <Tomb332@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-06-18 09:49:40 +0000

    wired user. Fires Ent use commands at objects in the beam. Useful for accessing buttons built into the map.

---
+++ wire/lua/entities/gmod_wire_user/init.lua
@@ -0,0 +1,59 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+ENT.WireDebugName = "User"
+
+local MODEL = Model("models/jaanus/wiretool/wiretool_siren.mdl")
+
+function ENT:Initialize()
+	self.Entity:SetModel( MODEL )
+	self.Entity:PhysicsInit( SOLID_VPHYSICS )
+	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
+	self.Entity:SetSolid( SOLID_VPHYSICS )
+	self.Inputs = Wire_CreateInputs(self.Entity, { "Fire"})
+	self.Outputs = Wire_CreateOutputs(self.Entity, {})
+    self:SetBeamRange(2048)
+end
+
+function ENT:OnRemove()
+	Wire_Remove(self.Entity)
+end
+
+function ENT:Setup(Range)
+	self:SetBeamRange(Range)
+	self:ShowOutput()
+end
+
+function ENT:TriggerInput(iname, value)
+	if (iname == "Fire") then
+		if (value ~= 0) then
+			 local vStart = self.Entity:GetPos()
+			 local vForward = self.Entity:GetUp()
+			 
+			 local trace = {}
+				 trace.start = vStart
+				 trace.endpos = vStart + (vForward * self:GetBeamRange())
+				 trace.filter = { self.Entity }
+			 local trace = util.TraceLine( trace ) 
+			
+			if (!trace.Entity) then return false end
+            if (!trace.Entity:IsValid() ) then return false end
+            if (trace.Entity:IsWorld()) then return false end
+            if ( CLIENT ) then return true end
+            trace.Entity:Fire("use","1",0)
+		end
+    end
+end
+
+function ENT:ShowOutput()
+	local text = "User"
+	self:SetOverlayText( text )
+end
+
+function ENT:OnRestore()
+    Wire_Restored(self.Entity)
+end
+   
