1a30596bf3587e0c618905dfb1b8823708f93f6d
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-06-30 23:18:50 +0000

    updates to the advanced pod controller by ShaRose

--- wire/lua/entities/gmod_wire_adv_pod/cl_init.lua
+++ wire/lua/entities/gmod_wire_adv_pod/cl_init.lua
@@ -1,11 +1,22 @@
-
 include('shared.lua')
 
 ENT.RenderGroup 		= RENDERGROUP_BOTH
+podinfo = {}
+ function CLAPI( um )
+	podinfo.string1 = um:ReadVector()
+	podinfo.string2 = um:ReadVector()
+	podinfo.string3 = um:ReadLong()
+ end
 
+usermessage.Hook("AdvPodInfo", CLAPI) 
 
 function ENT:Draw()
 	self.BaseClass.Draw(self)
-
+	if (podinfo.string3 == 1) then
+		self.Entity:DrawModel()
+		local laser = Material( "cable/redlaser" )
+		render.SetMaterial( laser )
+		render.DrawBeam( podinfo.string1, podinfo.string2, 2, 0, 12.5, Color(255, 0, 0, 255))
+	end
     Wire_Render(self.Entity)
 end
