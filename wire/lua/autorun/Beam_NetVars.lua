8308c1117f188814e72e0fd986f0aa22b8488f58
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-07-02 08:27:15 +0000

    this should fix the thruster fx sticking bug

--- wire/lua/autorun/Beam_NetVars.lua
+++ wire/lua/autorun/Beam_NetVars.lua
@@ -1,11 +1,52 @@
 AddCSLuaFile( "autorun/Beam_NetVars.lua" )
 
-//***********************************************************
-//		Get/Set Networked Beam Var
-//
-//	Basicly this one doesn't umsg.PoolString( Key )
-//	And has some other tweaks
-//***********************************************************
+///////////////////////////////////////////////
+//		===BeamNetVars===			//
+//	Custom Networked Vars Module			//
+//	Based off Garry's Networked Vars Module	//
+//	Modification by: TAD2020			//
+///////////////////////////////////////////////
+//	How to use:						//
+//	Just like NetVars, ent:SetNetworkedBeam*	//
+//	and ent:GetNetworkedBeam*			//
+//	Key should be short or a small number.		//
+//	These functions should only be used instead	//
+//	standard NetVars when very large quanity	//
+//	of rarly updated values need to be sent with	//
+//	low importantance. Mainly this is used by	//
+//	wire's client side beams and rapidly updating	//
+//	overlay text.					//
+//	Data is sent at a tick interval, if too much	//
+//	is in the outgoing query, the delay between	//
+//	sends is increased.					//
+//	On player joins, all current data is queried to//
+//	to send to them in the lowest priority stack.	//
+//	Low priority stack sends a few entities's	//
+//	vars each tick.					//
+///////////////////////////////////////////////
+
+//RD header for multi distro-ablity
+local ThisBeamNetVarsVersion = 0.71
+if (BeamNetVars) and (BeamNetVars.Version) and (BeamNetVars.Version > ThisBeamNetVarsVersion) then
+	Msg("======== A Newer Version of BeamNetVars Detected ========\n"..
+		"======== This ver: "..ThisBeamNetVarsVersion.." || Detected ver: "..BeamNetVars.Version.." || Skipping\n")
+	return
+elseif (BeamNetVars) and (BeamNetVars.Version) and (BeamNetVars.Version == ThisBeamNetVarsVersion) then
+	Msg("======== The Same Version of BeamNetVars Detected || Skipping ========\n")
+	return
+elseif (BeamNetVars) and (BeamNetVars.Version) then
+	Msg("======== Am Older Version of BeamNetVars Detected ========\n"..
+		"======== This ver: "..ThisBeamNetVarsVersion.." || Detected ver: "..BeamNetVars.Version.." || Overriding\n")
+end
+
+BeamNetVars = {}
+BeamNetVars.Version = ThisBeamNetVarsVersion
+
+if (SERVER) then
+	//we want this
+	//sv_usermessage_maxsize = 1024
+	game.ConsoleCommand( "sv_usermessage_maxsize 1024\n" )
+end
 
 local meta = FindMetaTable( "Entity" )
 
@@ -20,72 +61,51 @@ local NetworkVars 			= {}
 local NetworkFunction 	= {}
 local DelayedUpdates 	= {}
 local DelayedUpdatesNum = 0
+local ExtraDelayedUpdates = {}
 
 local NextCleanup		= CurTime()
 
 if ( CLIENT ) then
-
 	local function Dump()
-	
 		Msg("Networked Beam Vars...\n")
 		PrintTable( NetworkVars )
-		
 	end
-	
 	concommand.Add( "networkbeamvars_dump", Dump )
-
 end
 
 local function AttemptToSwitchTables( Ent, EntIndex )
-
 	if ( NetworkVars[ EntIndex ] == nil ) then return end
-
 	// We have an old entindex based entry! Move it over!
 	NetworkVars[ Ent ] = NetworkVars[ EntIndex ]
 	NetworkVars[ EntIndex ] = nil
-	
 end
 
 local function CleaupNetworkVars()
-
 	if ( NextCleanup > CurTime() ) then return end
-	NextCleanup	= CurTime() + 2
-
+	NextCleanup	= CurTime() + 30
 	for k, v in pairs( NetworkVars ) do
-	
 		if ( type( k ) != "number" && type( k ) != "string" ) then
-		
 			if ( !k:IsValid() ) then
 				NetworkVars[ k ] = nil
 			end
-		
 		end
-	
 	end
-
 end
 
 local function GetNetworkTable( ent, name )
-	
 	if ( CLIENT ) then
 		CleaupNetworkVars()
 	end
-	
 	if ( !NetworkVars[ ent ] ) then
-	
 		NetworkVars[ ent ] = {}
-		
 		// This is the first time this entity has been created. 
 		// Check whether we previously had an entindex based table
 		if ( CLIENT && type( ent ) != "number" && type( ent ) != "string" ) then
 			AttemptToSwitchTables( ent, ent:EntIndex() )
 		end
-	
 	end
-	
 	NetworkVars[ ent ][ name ] = NetworkVars[ ent ][ name ] or {}
 	return NetworkVars[ ent ][ name ]
-
 end
 
 local function SendNetworkUpdate( VarType, Index, Key, Value, Player )
@@ -96,22 +116,44 @@ local function SendNetworkUpdate( VarType, Index, Key, Value, Player )
 		umsg[ NetworkFunction[VarType].SetFunction ]( Value )
 	umsg.End()
 	
-	//umsg.PoolString( Key )
 
 end
 
 local function AddDelayedNetworkUpdate( VarType, Ent, Key, Value )
 	if (Wire_FastOverlayTextUpdate) then
 		SendNetworkUpdate( VarType, Ent, Key, Value )
-	else
-		DelayedUpdates[Ent] = DelayedUpdates[Ent] or {}
-		//DelayedUpdates[ VarType ] = DelayedUpdates[ VarType ] or {}
-		DelayedUpdates[Ent][ VarType ] = DelayedUpdates[Ent][ VarType ] or {}
-		DelayedUpdates[Ent][ VarType ][Key] = Value
-		DelayedUpdatesNum = DelayedUpdatesNum + 1
+	elseif (Ent) and (VarType) then
+		DelayedUpdates[Ent]					= DelayedUpdates[Ent] or {}
+		DelayedUpdates[Ent][ VarType ]		= DelayedUpdates[Ent][ VarType ] or {}
+		DelayedUpdates[Ent][ VarType ][Key]	= Value
+		DelayedUpdatesNum					= DelayedUpdatesNum + 1
+		
+		if (ExtraDelayedUpdates[Ent])
+		and (ExtraDelayedUpdates[Ent][VarType])
+		and (ExtraDelayedUpdates[Ent][VarType][Key]) then
+			ExtraDelayedUpdates[Ent][VarType][Key] = nil
+		end
 	end
 end
 
+local function AddExtraDelayedNetworkUpdate( VarType, Ent, Key, Value, Player )
+	if (Wire_FastOverlayTextUpdate) then
+		SendNetworkUpdate( VarType, Ent, Key, Value )
+	elseif (Ent) and (VarType) and (Key) then
+		ExtraDelayedUpdates[Ent]						= ExtraDelayedUpdates[Ent] or {}
+		ExtraDelayedUpdates[Ent][VarType]				= ExtraDelayedUpdates[Ent][VarType] or {}
+		ExtraDelayedUpdates[Ent][VarType][Key]			= ExtraDelayedUpdates[Ent][VarType][Key] or {}
+		ExtraDelayedUpdates[Ent][VarType][Key].Value	= Value
+		ExtraDelayedUpdates[Ent][VarType][Key].Player	= Player
+		--ExtraDelayedUpdatesNum = ExtraDelayedUpdatesNum + 1
+	end
+end
+
+
+
+//
+// make all the ent.Get/SetNetworkedBeamVarCrap
+//
 local function AddNetworkFunctions( name, SetFunction, GetFunction, Default )
 
 	NetworkFunction[ name ] = {}
@@ -205,38 +247,26 @@ local function AddNetworkFunctions( name, SetFunction, GetFunction, Default )
 	
 	// Client Receive Function
 	if ( CLIENT ) then
-	
-		local function RecvFunc( m )
 		
+		local function RecvFunc( m )
 			local EntIndex 	= m:ReadShort()
 			local Key		= m:ReadString()
 			local Value		= m[GetFunction]( m )
 			
 			local IndexKey
-			
 			if ( EntIndex <= 0 ) then 
-			
-				IndexKey = "G" 
-				
+				IndexKey = "G"
 			else
-			
 				IndexKey = Entity( EntIndex )
-				
 				// No entity yet - store using entindex
 				if ( IndexKey == NULL ) then IndexKey = EntIndex end
-				
 			end
-			
 			GetNetworkTable( IndexKey, name )[Key] = Value	
-			
-			//Msg("RECV: "..EntIndex.." | "..tostring(IndexKey).." - "..tostring(Key).." - "..tostring(Value).."\n");
-			
 		end
-		
 		usermessage.Hook( "RcvEntityVarBeam_"..name, RecvFunc )
-	
+		
 	end
-
+	
 end
 
 AddNetworkFunctions( "Vector", 	"Vector", 	"ReadVector", 	Vector_Default )
@@ -247,120 +277,49 @@ AddNetworkFunctions( "Entity", 	"Entity", 	"ReadEntity", 	NULL )
 AddNetworkFunctions( "Bool", 	"Bool", 	"ReadBool", 	false )
 AddNetworkFunctions( "String", 	"String", 	"ReadString", 	"" )
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Send a networkvar staggered to avoid sending too much in one tick
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-local function StaggeredNetworkUpdate( Type, Index, Key, Value, ply )
-
-	SendNetworkUpdate( Type, Index , Key, Value, ply )
-
-end
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Send a full update to player that have just joined the server
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-local function FullUpdateEntityNetworkVars( ply )
-
-	for Ent, EntTable in pairs(NetworkVars) do
-	
-		for Type, TypeTable in pairs(EntTable) do
-		
-			for Key, Value in pairs(TypeTable) do
-			
-				local Index = Ent
-				
-				if ( type(Ent) != "string" ) then
-					Index = Ent:EntIndex()
-				end
-			
-				SendNetworkUpdate( Type, Index , Key, Value, ply )
-							
-			end
-		end
-
-	end
 
 
-end
-local function DelayedFullUpdateEntityNetworkVars( ply )
-	--Msg("==starting timer for sending var data too "..tostring(ply).."\n")
-	timer.Simple(2, FullUpdateEntityNetworkVars, ply)
-end
---hook.Add( "PlayerInitialSpawn", "FullUpdateEntityNetworkBeamVars", FullUpdateEntityNetworkVars )
-hook.Add( "PlayerInitialSpawn", "FullUpdateEntityNetworkBeamVars", DelayedFullUpdateEntityNetworkVars )
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
+
+//
 // We want our networked vars to save don't we? Yeah - we do - stupid.
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
+//
 local function Save( save )
-
 	// Remove baggage
 	for k, v in pairs(NetworkVars) do
 		if ( k == NULL ) then
 			NetworkVars[k] = nil
 		end
 	end
-	
-	//PrintTable(NetworkVars)
 	saverestore.WriteTable( NetworkVars, save )
-
 end
-
 local function Restore( restore )
-
 	NetworkVars = saverestore.ReadTable( restore )
 	//PrintTable(NetworkVars)
-
 end
-
 saverestore.AddSaveHook( "EntityNetworkedBeamVars", Save )
 saverestore.AddRestoreHook( "EntityNetworkedBeamVars", Restore )
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
-// We update the vars periodically instead of on demand, because we're not made of bandwidth
-// Do you think it grows on trees or something? You'll see. You'll find out when you get your
-// own job and you come home every night and the fire is on full blast.
-///////////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////////
+if (SERVER) then
+//
+// send the netvars queried in the stack
+//
 local NextBeamVarsDelayedSendTime = 0
 local NormalOpMode = true
 local function NetworkVarsSend()
-	if (CurTime() >= NextBeamVarsDelayedSendTime) and (DelayedUpdatesNum > 0) then
+	if (CurTime() >= NextBeamVarsDelayedSendTime) then
 		
-		if (NormalOpMode) and (DelayedUpdatesNum > 50) then
-			
-			--Msg("========BeamVars leaving NormalOpMode | "..DelayedUpdatesNum.."\n")
+		if (NormalOpMode) and (DelayedUpdatesNum > 75) then
+			Msg("========BeamVars leaving NormalOpMode | "..DelayedUpdatesNum.."\n")
 			NormalOpMode = false
-			--when a shit load has be added, delay for a few seconds to allow other things to calm down
-			NextBeamVarsDelayedSendTime = CurTime() +  2
-			--Msg("=====BeamVars delay 2======\n")
-			return
-			
-		elseif (!NormalOpMode) and (DelayedUpdatesNum < 50) then
-			
+		elseif (!NormalOpMode) and (DelayedUpdatesNum < 50)  then
+			Msg("========BeamVars returning NormalOpMode | "..DelayedUpdatesNum.."\n")
 			NormalOpMode = true
-			--Msg("BeamVars retruning to NormalOpMode\n")
-			
 		end
 		
 		
-		
-		
-		if (NormalOpMode) then --during normal mode, we send the whole buffer every 0.05 sec
-			
-			
+		if (DelayedUpdatesNum > 0) then
 			for Index, a in pairs(DelayedUpdates) do
 				for VarType, b in pairs(a) do
 					for Key, Value in pairs(b) do
@@ -368,72 +327,76 @@ local function NetworkVarsSend()
 					end
 				end
 			end
-			DelayedUpdates = {}
 			DelayedUpdatesNum = 0
-			NextBeamVarsDelayedSendTime = CurTime() +  .1
-			
-		else --otherswise send 10 every 1/4 sec
-			
-			--Msg("BeamVars sending non-NormalOpMode data | "..DelayedUpdatesNum.."\n")
-			
-			local i = 0
-			/*for VarType, a in pairs(DelayedUpdates) do
-				for Index, b in pairs(a) do*/
-			for Index, a in pairs(DelayedUpdates) do
-				for VarType, b in pairs(a) do
-					for Key, Value in pairs(b) do
-						SendNetworkUpdate( VarType, Index, Key, Value )
-						DelayedUpdatesNum = DelayedUpdatesNum - 1
-					end
-					
-				end
-				
-				a = nil
-				
-				i = i + 1
-				if i > 2 then
-					NextBeamVarsDelayedSendTime = CurTime() +  .25
-					return
-				end
-			end
-			
-			--Msg("BeamVars retruning to NormalOpMode from not\n")
-			NormalOpMode = true
-			NextBeamVarsDelayedSendTime = CurTime() +  .1
-			
+			DelayedUpdates = {}
 		end
 		
-		
-		/*for VarType, a in pairs(DelayedUpdates) do
-			for Index, b in pairs(a) do
-				for Key, Value in pairs(b) do
-					SendNetworkUpdate( VarType, Index, Key, Value )
+		//we send one entity's ExtraDelayedUpdates each tick
+		local i = 0
+		for Index, a in pairs(ExtraDelayedUpdates) do
+			for VarType, b in pairs(a) do
+				for Key, data in pairs(b) do
+					SendNetworkUpdate( VarType, Index, Key, data.Value, data.Player )
 				end
+			end
+			ExtraDelayedUpdates[Index] = nil
+			i = i + 1
+			if (i >= 2) then
+				break
 			end
 		end
 		
-		// Clear the sent entries
-		DelayedUpdates = {}*/
+		if (!NormalOpMode) then
+			NextBeamVarsDelayedSendTime = CurTime() +  .25
+		else
+			NextBeamVarsDelayedSendTime = CurTime() +  .1
+		end
 		
-		//NextBeamVarsDelayedSendTime = CurTime() +  .1
 	end
 end
-//timer.Create( "NetworkBeamVarsSend", 0.01, 0, NetworkVarsSend )
 hook.Add("Think", "NetBeamLib_Think", NetworkVarsSend)
 
 
+//
+// Send a full update to player that have just joined the server
+//
+local function FullUpdateEntityNetworkVars( ply )
+	--Msg("==sending netbeamvar var data to "..tostring(ply).."\n")
+	--Msg("\n===Size: "..table.Count(NetworkVars).."\n")
+	for Ent, EntTable in pairs(NetworkVars) do
+		for Type, TypeTable in pairs(EntTable) do
+			for Key, Value in pairs(TypeTable) do
+				local Index = Ent
+				if ( type(Ent) != "string" ) then
+					Index = Ent:EntIndex()
+				end
+				--SendNetworkUpdate( Type, Index , Key, Value, ply )
+				AddExtraDelayedNetworkUpdate( Type, Index , Key, Value, ply )
+			end
+		end
 
-///////////////////////////////////////////////////////////////////////////////////////////////
-// Listen out for dead entities so we can remove their vars
-///////////////////////////////////////////////////////////////////////////////////////////////
-local function NetworkVarsCleanup( ent )
-
-	if ( SERVER ) then
-		NetworkVars[ ent ] = nil
 	end
-
 end
+local function DelayedFullUpdateEntityNetworkVars( ply )
+	--Msg("==starting timer for sending var data too "..tostring(ply).."\n")
+	timer.Simple(4, FullUpdateEntityNetworkVars, ply)
+end
+hook.Add( "PlayerInitialSpawn", "FullUpdateEntityNetworkBeamVars", DelayedFullUpdateEntityNetworkVars )
+concommand.Add( "networkbeamvars_SendAll", DelayedFullUpdateEntityNetworkVars )
+concommand.Add( "networkbeamvars_SendAllNow", FullUpdateEntityNetworkVars )
 
+
+//
+// Listen out for dead entities so we can remove their vars
+//
+local function NetworkVarsCleanup( ent )
+	NetworkVars[ ent ] = nil
+end
 hook.Add( "EntityRemoved", "NetworkBeamVarsCleanup", NetworkVarsCleanup )
 
 
+end //end SERVER olny
+
+
+
+Msg("======== Beam NetVars Lib v"..BeamNetVars.Version.." Installed ========\n")
