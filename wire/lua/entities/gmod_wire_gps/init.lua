2c815b65a3eb858ffa830f0de2c80a08902c04ea
LuaPineapple <LuaPineapple@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-07-03 18:53:26 +0000

    Added deployable balloon SENT V2. Updated target finder (Nicks, NPC type, ENT Scan). Updated GPS (Built in memory). Added unpopable balloon (for deployer).

--- wire/lua/entities/gmod_wire_gps/init.lua
+++ wire/lua/entities/gmod_wire_gps/init.lua
@@ -13,7 +13,11 @@ function ENT:Initialize()
 	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
 	self.Entity:SetSolid( SOLID_VPHYSICS )
 
-	self.Outputs = Wire_CreateOutputs(self.Entity, { "X", "Y", "Z" })
+	self.storedpositions = {};
+	self.arrayindex = 0;
+	
+	self.Inputs = Wire_CreateInputs(self.Entity, { "Store/Save Pos", "Next", "Remove Save Position"})
+	self.Outputs = Wire_CreateOutputs(self.Entity, { "X", "Y", "Z", "Recall X", "Recall Y", "Recall Z", "Current Memory"})
 end
 
 function ENT:Setup()
@@ -24,6 +28,10 @@ function ENT:Setup()
 	Wire_TriggerOutput(self.Entity, "X", 0)
 	Wire_TriggerOutput(self.Entity, "Y", 0)
 	Wire_TriggerOutput(self.Entity, "Z", 0)
+	Wire_TriggerOutput(self.Entity, "Recall X", 0)
+	Wire_TriggerOutput(self.Entity, "Recall Y", 0)
+	Wire_TriggerOutput(self.Entity, "Recall Z", 0)
+	Wire_TriggerOutput(self.Entity, "Current Memory", 0)
 end
 
 function ENT:Think()
@@ -33,13 +41,49 @@ function ENT:Think()
 	Wire_TriggerOutput(self.Entity, "X", pos.x)
 	Wire_TriggerOutput(self.Entity, "Y", pos.y)
 	Wire_TriggerOutput(self.Entity, "Z", pos.z)
-	//moe to client side (TAD2020)
-	//self:ShowOutput(pos.x, pos.y, pos.z)
+	Wire_TriggerOutput(self.Entity, "Current Memory", self.arrayindex)
+	if self.arrayindex > 0 then
+		Wire_TriggerOutput(self.Entity, "Recall X", self.storedpositions[self.arrayindex].z)
+		Wire_TriggerOutput(self.Entity, "Recall Y", self.storedpositions[self.arrayindex].y)
+		Wire_TriggerOutput(self.Entity, "Recall Z", self.storedpositions[self.arrayindex].z)
+	else
+		Wire_TriggerOutput(self.Entity, "Recall X", 0)
+		Wire_TriggerOutput(self.Entity, "Recall Y", 0)
+		Wire_TriggerOutput(self.Entity, "Recall Z", 0)
+	end
 	
 	self.Entity:NextThink(CurTime()+0.04)
 	return true
 end
 
-/*function ENT:ShowOutput(x, y, z)
-	self:SetOverlayText( "Position = " .. math.Round(x*1000)/1000 .. "," .. math.Round(y*1000)/1000 .. "," .. math.Round(z*1000)/1000 )
-end*/
+function ENT:TriggerInput(iname, value)
+	if (iname == "Store/Save Pos") then
+		if (value ~= 0) then
+			local curpos = self.Entity:GetPos()
+			table.insert(self.storedpositions, curpos)
+			self.arrayindex = self.arrayindex+1
+		end
+	elseif (iname == "Next") then
+		if (value ~= 0) then
+			if # self.storedpositions > 0 then
+				if not (self.arrayindex >= # self.storedpositions) then
+					self.arrayindex = self.arrayindex+1;
+				else
+					self.arrayindex = 1; --loop back
+				end
+			end
+		end
+	elseif (iname == "Remove Save Position") then
+		if (value ~= 0) then
+			if self.arrayindex ~= 0 then
+				table.remove(self.storedpositions, self.arrayindex)
+			end
+			if (self.arrayindex == 1) and (# self.storedpositions == 0) then
+				self.arrayindex = 0
+			end
+			if (self.arrayindex == (# self.storedpositions+1)) then
+				self.arrayindex = self.arrayindex-1
+			end
+		end
+	end
+end
---
+++ wire/lua/entities/sent_deployableballoons/shared.lua
@@ -0,0 +1,7 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_wire_entity"
+ENT.PrintName			= "Balloon Deployer"
+ENT.Author			= "LuaPinapple"
+ENT.Contact			= "evilpineapple@cox.net"
+ENT.Purpose			= "Figure it out man."
+ENT.Instructions		= "Use wire."
