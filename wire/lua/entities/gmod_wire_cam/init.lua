c6d5b0840382dc6a80e7ea91fd53ee155bc7585b
tomb <tomb@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-12-02 12:19:44 +0000

    [ADDED] Wire Camera Controller. VERY IMPORTANT POINT. To set the person the camera works for press use on it. (defaults to owner on spawn)

---
+++ wire/lua/entities/gmod_wire_cam/init.lua
@@ -0,0 +1,41 @@
+
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
+
+include('shared.lua')
+
+ENT.WireDebugName = "Camera"
+
+function ENT:Initialize()
+    self.IdealPos = self:GetPos()
+    self.IdealAng = self:GetAngles()
+end
+
+function ENT:Think()
+    if(self:GetPos() != self.IdealPos)then
+        self:SetPos(self.IdealPos)
+    end
+    if(self:GetAngles() != self.IdealAng)then
+        self:SetAng(self.IdealAng)
+    end
+    if(self:GetColor() != Color(0,0,0,0))then
+        self:SetColor(0,0,0,0)
+    end
+    self:NextThink(CurTime()+0.25)
+end
+
+function ENT:RecieveInfo(Pos,Ang)
+    self.IdealPos = Pos
+    self:SetPos(Pos)
+    self.IdealAng = Ang
+    self:SetAngles(Ang)
+end
+
+function ENT:OnRemove()
+	Wire_Remove(self.Entity)
+end
+
+function ENT:OnRestore()
+    Wire_Restored(self.Entity)
+end
+   
