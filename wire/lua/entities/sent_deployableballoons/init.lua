93d02525f7176f40e72e2159d977eb263c96b9c1
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2007-12-29 08:09:28 +0000

    [fixed] balloon deployer breaking all its constraints [added] balloon pop effect [added] popable input (makes a normal balloon that can be popped instead)

--- wire/lua/entities/sent_deployableballoons/init.lua
+++ wire/lua/entities/sent_deployableballoons/init.lua
@@ -23,11 +23,11 @@ function ENT:Initialize()
 	self.Balloon = 0
 	self.Constraints = {}
 	self.force = 500
-	self.weld = 0
+	self.weld = false
+	self.popable = false
 	self.rl = 64
-	self.amount = 1 --should not change
 	if WireAddon then
-		self.Inputs = Wire_CreateInputs(self.Entity,{ "Deploy", "Force", "Lenght", "Weld?"})
+		self.Inputs = Wire_CreateInputs(self.Entity,{ "Deploy", "Force", "Lenght", "Weld?", "Popable?"})
 		self.Outputs = Wire_CreateOutputs(self.Entity,{ "Deployed"})
 		Wire_TriggerOutput(self.Entity,"Deployed", self.Deployed)
 		Wire_TriggerOutput(self.Entity,"Force", self.force)
@@ -62,53 +62,57 @@ function ENT:TriggerInput(key,value)
 	elseif (key == "Lenght") then
 		self.rl = value
 	elseif (key == "Weld?") then
-		if value > 0 then
-			self.weld = 1
-		else
-			self.weld = 0
-		end
+		self.weld = value > 0
+	elseif (key == "Popable?") then
+		self.popable = value > 0
 	end
 end
 
 function ENT:DeployBalloons()
-	self.BalloonsAndStuff = {} --reset array
-	for i=1,self.amount do
-		local balloon = ents.Create("gmod_iballoon") --invincible balloon
-		balloon:Spawn()
-		balloon:SetRenderMode( RENDERMODE_TRANSALPHA )
-		balloon:SetColor(math.random(0,255), math.random(0,255), math.random(0,255), 255 )
-		balloon:SetForce(self.force)
-		balloon:SetMaterial("models/balloon/balloon")
-		duplicator.DoGeneric(balloon,{Pos = self.Entity:GetPos() + (self.Entity:GetUp()*25)})
-		duplicator.DoGenericPhysics(balloon,pl,{Pos = Pos})
-		local spawnervec = (self.Entity:GetPos()-balloon:GetPos()):Normalize()*250 --just to be sure
-		local trace = util.QuickTrace(balloon:GetPos(),spawnervec,balloon)
-		local Pos = self.Entity:GetPos()+(self.Entity:GetUp()*25)
-		local attachpoint = Pos + Vector(0,0,-10)	
-		local LPos1 = balloon:WorldToLocal(attachpoint)
-		local LPos2 = trace.Entity:WorldToLocal(trace.HitPos)
-		local phys = trace.Entity:GetPhysicsObjectNum(trace.PhysicsBone)
-		if(phys:IsValid()) then
-			LPos2 = phys:WorldToLocal(trace.HitPos)
-		end
-		if self.weld == 1 then
-			local constraint = constraint.Weld( balloon, trace.Entity, 0, trace.PhysicsBone, 0)
-			table.insert(self.Constraints,constraint)
-		else
-			local constraint, rope = constraint.Rope(balloon,trace.Entity,0,trace.PhysicsBone,LPos1,LPos2,0,self.rl,0,1.5,material,nil)
-			table.insert(self.Constraints,constraint)
-			table.insert(self.Constraints,rope)
-		end
-		self.Balloon = balloon
+	local balloon
+	if self.popable then
+		balloon = ents.Create("gmod_balloon") --normal balloon
+	else
+		balloon = ents.Create("gmod_iballoon") --invincible balloon
 	end
+	balloon:Spawn()
+	balloon:SetRenderMode( RENDERMODE_TRANSALPHA )
+	balloon:SetColor(math.random(0,255), math.random(0,255), math.random(0,255), 255 )
+	balloon:SetForce(self.force)
+	balloon:SetMaterial("models/balloon/balloon")
+	duplicator.DoGeneric(balloon,{Pos = self.Entity:GetPos() + (self.Entity:GetUp()*25)})
+	duplicator.DoGenericPhysics(balloon,pl,{Pos = Pos})
+	local spawnervec = (self.Entity:GetPos()-balloon:GetPos()):Normalize()*250 --just to be sure
+	local trace = util.QuickTrace(balloon:GetPos(),spawnervec,balloon)
+	local Pos = self.Entity:GetPos()+(self.Entity:GetUp()*25)
+	local attachpoint = Pos + Vector(0,0,-10)	
+	local LPos1 = balloon:WorldToLocal(attachpoint)
+	local LPos2 = trace.Entity:WorldToLocal(trace.HitPos)
+	local phys = trace.Entity:GetPhysicsObjectNum(trace.PhysicsBone)
+	if(phys:IsValid()) then
+		LPos2 = phys:WorldToLocal(trace.HitPos)
+	end
+	if self.weld then
+		local constraint = constraint.Weld( balloon, trace.Entity, 0, trace.PhysicsBone, 0)
+		balloon:DeleteOnRemove(constraint)
+	else
+		local constraint, rope = constraint.Rope(balloon,trace.Entity,0,trace.PhysicsBone,LPos1,LPos2,0,self.rl,0,1.5,material,nil)
+		balloon:DeleteOnRemove(constraint)
+		balloon:DeleteOnRemove(rope)
+	end
+	self.Entity:DeleteOnRemove(balloon)
+	self.Balloon = balloon
 end
 
 function ENT:RetractBalloons()
-	for k,v in pairs(self.Constraints) do
-		self.Constraints[k] = nil;
-		if(v and v:IsValid()) then
-			v:Remove()
-		end
+	if self.Balloon:IsValid() then
+		local r, g, b = self.Balloon:GetColor()
+		local effectdata = EffectData()
+			effectdata:SetOrigin( self.Balloon:GetPos() )
+			effectdata:SetStart( Vector( r, g, b ) )
+		util.Effect( "balloon_pop", effectdata )
+		self.Balloon:Remove()
+	else
+		self.Balloon = nil
 	end
-	self.Balloon:Remove()
 end
