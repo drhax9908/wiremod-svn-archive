0c848389045334b2d6f1c2934f6ca9bdc338e821
tad2020 <tad2020@21c5dc87-7110-4d66-9237-d0800a75ba64>
2008-01-06 10:17:03 +0000

    [added] pod relitive to bearing and elevation (enable via input) [added] ablity to push wire buttons from pod (enable via input) [fixed] garbage output when entering pod (does not output aim data for 2 seconds when entering) [fixed] x,y,z aim data not work on hl2 pod [fixed] outputs not clearing after leaving pod [added] 3 keys Alt(walk), TurnLeltKey, and TurnRightKey (define last 2 first, but they do cause you to turn so it may not be that useful) [changed] Key outputs now sorted as such for ease: W,A,S,D,Mouse1,Mouse2,R,Space,Ctrl,Shift,Zoom,Alt,Left,Right [problem] Ctrl(crouch) doesn't work in gmod2007 because that key gets rebound for toggeling 3rd person view

--- wire/lua/entities/gmod_wire_adv_pod/init.lua
+++ wire/lua/entities/gmod_wire_adv_pod/init.lua
@@ -11,7 +11,7 @@ function ENT:Initialize()
 	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
 	self.Entity:SetSolid( SOLID_VPHYSICS )
 	self.lockvar = 0
-	self.disablevar = 0
+	self.disablevar = false
 	self.crossvar = 0
 	-- Output keys. Format: self.keys["name"] = IN_*
 	self.keys = { }
@@ -26,21 +26,36 @@ function ENT:Initialize()
 	self.keys["Ctrl"] = IN_DUCK
 	self.keys["Shift"] = IN_SPEED
 	self.keys["Zoom"] = IN_ZOOM
-	-- Invert the table to use it with Wire_CreateOutputs
+	self.keys["Alt(walk)"] = IN_WALK
+	self.keys["TurnLeltKey"] = IN_LEFT
+	self.keys["TurnRightKey"] = IN_RIGHT
+
 	local outputs = { }
-	local n = 1
-	for k, v in pairs( self.keys ) do
-		outputs[n] = k
-		n = n + 1
-	end
+	outputs[1] = "W"
+	outputs[2] = "A"
+	outputs[3] = "S"
+	outputs[4] = "D"
+	outputs[5] = "Mouse1"
+	outputs[6] = "Mouse2"
+	outputs[7] = "R"
+	outputs[8] = "Space"
+	outputs[9] = "Ctrl"
+	outputs[10] = "Shift"
+	outputs[11] = "Zoom"
+	outputs[12] = "Alt(walk)"
+	outputs[13] = "TurnLeltKey"
+	outputs[14] = "TurnRightKey"
 	
-	-- <angle>
+	local n = 15
+	
+	--aim
 	outputs[n] = "X"
 	n = n + 1
 	outputs[n] = "Y"
 	n = n + 1
 	outputs[n] = "Z"
-	-- </angle>
+	--n = n + 1
+	--outputs[n] = "AimVector" --TODO: place holder for later
 	
 	n = n + 1
 	outputs[n] = "Distance"
@@ -65,8 +80,12 @@ function ENT:Initialize()
 	self.VPos = Vector(0, 0, 0)
 	
 	self.Outputs = Wire_CreateOutputs( self.Entity, outputs )
-	self.Inputs = Wire_CreateInputs( self.Entity, { "Lock", "Terminate", "Strip weapons", "Eject", "Disable", "Crosshairs", "Brake"} )
+	self.Inputs = Wire_CreateInputs( self.Entity, { "Lock", "Terminate", "Strip weapons", "Eject", "Disable", "Crosshairs", "Brake", "Allow Buttons", "Relitive"} )
 	self:SetOverlayText( "Adv. Pod Controller" )
+	
+	self.pushbuttons = false
+	self.LastPressed = 0
+	self.BE_rel = false
 end
 
 function ENT:Setup(pod)
@@ -105,13 +124,7 @@ function ENT:TriggerInput(iname, value)
 				end
 			end
 		elseif (iname == "Disable") then
-			if (value >= 1) then
-				if (self.disablevar == 0) then
-					self.disablevar = 1
-				else
-					self.disablevar = 0
-				end
-			end
+			self.disablevar = (value >= 1)
 		elseif (iname == "Crosshairs") then
 			if (value >= 1) then
 				if self.Ply and self.Ply:IsValid() then
@@ -132,6 +145,10 @@ function ENT:TriggerInput(iname, value)
 				self.Pod:Fire("TurnOn", "1", 0)
 				self.Pod:Fire("HandBrakeOff", "1", 0)
 			end
+		elseif (iname == "Allow Buttons") then
+			self.pushbuttons = (value >= 1)
+		elseif (iname == "Relitive") then
+			self.BE_rel = (value >= 1)
 		end
 end
 
@@ -158,44 +175,94 @@ function ENT:OnRestore()
     self.BaseClass.OnRestore(self)
 end
 
+local function fixupangle(angle)
+	if angle > 180 then angle = angle - 360 end
+	if angle < -180 then angle = angle + 360 end
+	return angle
+end
+
 function ENT:Think()
 	if self.Pod then
-		self.Ply = self.Pod:GetPassenger()
-		if self.Ply and self.Ply:IsValid() then
+		local Ply = self.Pod:GetPassenger()
+		if Ply and Ply:IsValid() then
+			if self.Ply == nil then self.junkBE = CurTime() + 2 end
+			self.Ply = Ply
+			
 			Wire_TriggerOutput( self.Entity, "Active", 1)
-			for k, v in pairs( self.keys )  do
-				if self.Ply:KeyDownLast( v ) then Wire_TriggerOutput( self.Entity, k, 1 )
-				else Wire_TriggerOutput( self.Entity, k, 0 ) end
+			
+			if not self.disablevar then
+				for k, v in pairs( self.keys )  do
+					if not self.disablevar and self.Ply:KeyDownLast( v ) then
+						Wire_TriggerOutput( self.Entity, k, 1 )
+					else
+						Wire_TriggerOutput( self.Entity, k, 0 )
+					end
+				end
 			end
-			local MyPos = self.Ply:GetShootPos()
-			local viewangle = self.Ply:GetAimVector():Angle()
-			local tmp2 = self.Ply:GetEyeTrace()
-			tmp2.Filter = self.Pod
-			local tmp = tmp2.HitPos
-			Wire_TriggerOutput( self.Entity, "X", tmp.x )
-			Wire_TriggerOutput( self.Entity, "Y", tmp.y )
-			Wire_TriggerOutput( self.Entity, "Z", tmp.z )
-			local dist = (tmp-MyPos):Length()
-			Wire_TriggerOutput( self.Entity, "Distance", dist)
-			local plyteam = self.Ply:Team( )
-			Wire_TriggerOutput( self.Entity, "Team", plyteam)
-			local plyheal = self.Ply:Health( )
-			Wire_TriggerOutput( self.Entity, "Health", plyheal)
-			local plyarm = self.Ply:Armor( )
-			Wire_TriggerOutput( self.Entity, "Armor", plyarm)
-			self.VPos = tmp
-			local pitch = -viewangle.p
-			if pitch > 180 then pitch = pitch - 360 end
-			if pitch < -180 then pitch = pitch + 360 end
-			local yaw = viewangle.y
-			if yaw > 180 then yaw = yaw - 360 end
-			if yaw < -180 then yaw = yaw + 360 end
-			Wire_TriggerOutput(self.Entity, "Bearing", -(yaw + 90))
-			Wire_TriggerOutput(self.Entity, "Elevation", pitch)
+			
+			--player info
+			Wire_TriggerOutput( self.Entity, "Team", self.Ply:Team())
+			Wire_TriggerOutput( self.Entity, "Health", self.Ply:Health())
+			Wire_TriggerOutput( self.Entity, "Armor", self.Ply:Armor())
+			
+			if self.junkBE then --all this info is garbage while the player is entering the pod, junk it for the first 2 second
+				if self.junkBE < CurTime() then self.junkBE = nil end
+			else
+				local trace = util.GetPlayerTrace( self.Ply )
+				trace.filter = {self.Ply,self.Pod}
+				local EyeTrace = util.TraceLine( trace )
+				self.VPos = EyeTrace.HitPos
+				local dist = (EyeTrace.HitPos-self.Ply:GetShootPos()):Length()
+				Wire_TriggerOutput( self.Entity, "Distance", dist)
+				
+				Wire_TriggerOutput( self.Entity, "X", EyeTrace.HitPos.x )
+				Wire_TriggerOutput( self.Entity, "Y", EyeTrace.HitPos.y )
+				Wire_TriggerOutput( self.Entity, "Z", EyeTrace.HitPos.z )
+				
+				local AimVectorAngle = self.Ply:GetAimVector():Angle()
+				local PodAngle = self.Pod:GetAngles()
+				if self.BE_rel then
+					Wire_TriggerOutput(self.Entity, "Bearing", fixupangle(AimVectorAngle.y - PodAngle.y))
+					Wire_TriggerOutput(self.Entity, "Elevation", fixupangle(AimVectorAngle.p - PodAngle.p))
+				else
+					Wire_TriggerOutput(self.Entity, "Bearing", -(fixupangle(AimVectorAngle.y) + 90))
+					Wire_TriggerOutput(self.Entity, "Elevation", fixupangle(-AimVectorAngle.p))
+				end
+				
+				if self.pushbuttons then
+					if EyeTrace.Entity and EyeTrace.Entity:IsValid() and EyeTrace.Entity:GetClass() == "gmod_wire_button" and dist < 256 and self.Ply:KeyDownLast( IN_ATTACK ) then
+						if EyeTrace.Entity.Toggle then
+							if self.LastPressed + 0.5 < CurTime() then
+								EyeTrace.Entity:Switch(not EyeTrace.Entity:IsOn())
+								self.LastPressed = CurTime()
+							end
+						elseif not EyeTrace.Entity:IsOn() then
+							EyeTrace.Entity:Switch(true)
+							EyeTrace.Entity.PrevUser = self.Ply
+							EyeTrace.Entity.podpress = true
+						end
+					end
+				end
+			end
 		else
-			Wire_TriggerOutput( self.Entity, "Active", 0) 
+			if self.Ply then --clear outputs
+				Wire_TriggerOutput( self.Entity, "Active", 0) 
+				for k, _ in pairs( self.keys )  do
+					Wire_TriggerOutput( self.Entity, k, 0 )
+				end
+				Wire_TriggerOutput( self.Entity, "Team", 0)
+				Wire_TriggerOutput( self.Entity, "Health", 0)
+				Wire_TriggerOutput( self.Entity, "Armor", 0)
+				Wire_TriggerOutput( self.Entity, "Distance", 0)
+				Wire_TriggerOutput( self.Entity, "X", 0)
+				Wire_TriggerOutput( self.Entity, "Y", 0)
+				Wire_TriggerOutput( self.Entity, "Z", 0)
+				Wire_TriggerOutput(self.Entity, "Bearing", 0)
+				Wire_TriggerOutput(self.Entity, "Elevation", 0)
+			end
+			self.Ply = nil
 		end
-		if (self.disablevar == 1) then
+		if self.disablevar then
 			for k, v in pairs( self.keys )  do
 				Wire_TriggerOutput( self.Entity, k, 0 )
 			end
