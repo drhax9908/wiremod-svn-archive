59e45de5ca1641e16e104eb4f901b5de2455609b
tomb <tomb@21c5dc87-7110-4d66-9237-d0800a75ba64>
2008-08-26 15:19:48 +0000

    Cam Update [FIX]Pod Link Duplication Should Work [FIX]Static Cams Should Now Duplicate Correctly [ADDED]Cams Now Output The Coordinates they are pointing at.

--- wire/lua/entities/gmod_wire_cameracontroller/init.lua
+++ wire/lua/entities/gmod_wire_cameracontroller/init.lua
@@ -10,7 +10,7 @@ function ENT:Initialize()
 	self.Entity:PhysicsInit( SOLID_VPHYSICS )
 	self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
 	self.Entity:SetSolid( SOLID_VPHYSICS )
-	self.Outputs = Wire_CreateOutputs(self.Entity, {"On"})
+	self.Outputs = Wire_CreateOutputs(self.Entity, {"On","X","Y","Z"})
     self.Active = false
     self.OriginalOwner = nil
     self.CamEnt = nil
@@ -41,18 +41,35 @@ function ENT:Setup(Player,Static)
 	   self.CamEnt = cam
         self.Inputs = Wire_CreateInputs(self.Entity, {"Activated","Zoom","X","Y","Z","Pitch","Yaw","Roll"})
 	else
-	   local cam = ents.Create("prop_physics")
-        if (!cam:IsValid()) then return false end
-
-	   cam:SetAngles( Vector(0,0,0) )
-	   cam:SetPos( self:GetPos()+Vector(0,0,64) )
-	   cam:SetModel( Model("models/dav0r/camera.mdl") )
-	   cam:Spawn()
-	
-	   self.CamEnt = cam
-	   	self.Inputs = Wire_CreateInputs(self.Entity, {"Activated","Zoom"})
+	   self.Inputs = Wire_CreateInputs(self.Entity, {"Activated","Zoom"})
 	   self.Static = 1
 	end
+end
+
+function ENT:Think()
+    self.BaseClass.Think(self)
+    
+    local vStart = self.CamEnt:GetPos()
+	local vForward = self.CamEnt:GetForward()
+			 
+	local trace = {}
+	trace.start = vStart
+	trace.endpos = vStart + (vForward * 100000)
+	trace.filter = { self.CamEnt }
+	local trace = util.TraceLine( trace )
+	
+	if(trace.HitPos)then
+	   Wire_TriggerOutput(self.Entity,"X",trace.HitPos.x)
+	   Wire_TriggerOutput(self.Entity,"Y",trace.HitPos.y)
+	   Wire_TriggerOutput(self.Entity,"Z",trace.HitPos.z)
+	else
+	   Wire_TriggerOutput(self.Entity,"X",0)
+	   Wire_TriggerOutput(self.Entity,"Y",0)
+	   Wire_TriggerOutput(self.Entity,"Z",0)
+	end
+    
+    self.Entity:NextThink(CurTime()+0.1)
+    return true 
 end
 
 function ENT:OnRemove()
@@ -125,4 +142,31 @@ end
 
 function ENT:OnRestore()
     Wire_Restored(self.Entity)
+end
+
+function ENT:BuildDupeInfo()
+	local info = self.BaseClass.BuildDupeInfo(self) or {}
+	if (self.CamPod) and (self.CamPod:IsValid()) then
+	    info.pod = self.CamPod:EntIndex()
+	end
+	if (self.CamEnt) and (self.CamEnt:IsValid()) and (self.Static == 1)then
+	   info.cam = self.CamEnt:EntIndex()
+	end
+	return info
+end
+
+function ENT:ApplyDupeInfo(ply, ent, info, GetEntByID)
+	self.BaseClass.ApplyDupeInfo(self, ply, ent, info, GetEntByID)
+	if (info.pod) then
+		self.CamPod = GetEntByID(info.pod)
+		if (!self.CamPod) then
+			self.CamPod = ents.GetByIndex(info.pod)
+		end
+	end
+	if (info.cam) then
+	   self.CamEnt = GetEntByID(info.cam)
+	   if(!self.CamEnt) then
+	       self.CamEnt = ents.GetByIndex(info.cam)
+	   end
+	end
 end
