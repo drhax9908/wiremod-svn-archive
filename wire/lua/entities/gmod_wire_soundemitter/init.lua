39294491319d4dcc418f5aafeb808c765b4128dd
BlackPhoenix <BlackPhoenix@21c5dc87-7110-4d66-9237-d0800a75ba64>
2008-10-14 16:09:17 +0000

    [FIXED] Caching problem in console screen [FIXED] Added new sound emitter based off the Qilers one [ADDED] High-speed inputs to sound emitter (I have no idea how fast they really work, so assume they work the same speed as regular wire)

--- wire/lua/entities/gmod_wire_soundemitter/init.lua
+++ wire/lua/entities/gmod_wire_soundemitter/init.lua
@@ -12,26 +12,11 @@ function ENT:Initialize()
 	self.Entity:SetSolid( SOLID_VPHYSICS )
 	
 	self.Inputs = Wire_CreateInputs(self.Entity, { "A", "Toggle", "Volume", "Play", "Stop", 
-		/*"PitchNote",*/ "PitchRelative", "PitchStart", 
-		"SpinUpTime", "SpinDownTime", "FadeInStartVolume", "FadeInTime", "FadeOutTime",
-		"LFOType", "LFORate", "LFOModPitch", "LFOModVolume", "Sample" })
+		"PitchRelative", "LFOType", "LFORate", "LFOModPitch", "LFOModVolume", "Sample" })
 
-	self.BaseFreq = 440
 	self.Active = 0
 	self.Volume = 5
 	self.Pitch = 100
-
-	self.SoundOutput = ents.Create("ambient_generic") //magic begins
-	self.SoundOutput:SetName("Wire_SoundOutput_"..self:EntIndex())
-	self.SoundOutput:SetPos(self.Entity:GetPos())
-	self.SoundOutput:SetOwner(self.Entity:GetOwner())
-	self.SoundOutput:SetKeyValue("message", "synth/square.wav")
-	self.SoundOutput:SetKeyValue("health", "5")
-	self.SoundOutput:SetKeyValue("spawnflags","16") //looped, and silent
-	self.SoundOutput:SetKeyValue("radius", "1024")
-
-	self.SoundOutput:Spawn()
-	self.SoundOutput:Activate()
 
 	self.SampleTable = {}
 	self.SampleTable[0] = "synth/square.wav"
@@ -56,10 +41,6 @@ function ENT:Initialize()
 
 	self.LFOValue = 0
 	self.LFONoiseTime = 0
-	
-
-//	expl:Fire("explode", "", 0)
-//	expl:Fire("kill","",0)
 
 //	note = 69+12 * log2(f/440)
 //	f = (2^((note - 69) / 12))*440
@@ -68,7 +49,37 @@ end
 function ENT:OnRemove()
 	self.BaseClass.OnRemove(self)
 	self:StopSounds()
-	self.SoundOutput:Fire("Kill", "", 0)
+end
+
+function ENT:ReadCell(Address)
+	if (Address < 0) || (Address > 8) then
+		return nil
+	else
+		return 0
+	end
+end
+
+function ENT:WriteCell(Address, value)
+	if (Address == 0) then
+		self:TriggerInput("A",value)
+	elseif (Address == 1) then
+		self:TriggerInput("Volume",value)
+	elseif (Address == 2) then
+		self:TriggerInput("PitchRelative",value)
+	elseif (Address == 3) then
+		self:TriggerInput("Sample",value)
+	elseif (Address == 4) then
+		self:TriggerInput("LFOType",value)
+	elseif (Address == 5) then
+		self:TriggerInput("LFORate",value)
+	elseif (Address == 6) then
+		self:TriggerInput("LFOModPitch",value)
+	elseif (Address == 7) then
+		self:TriggerInput("LFOModVolume",value)
+	else
+		return false
+	end
+	return true	
 end
 
 function ENT:TriggerInput(iname, value)
@@ -78,28 +89,35 @@ function ENT:TriggerInput(iname, value)
 		self.Active = active
 		if (active) then
 			self:StartSounds()
+			self.SND:ChangeVolume(self.Volume)
+			self.SND:ChangePitch(self.Pitch)
 		else
 			self:StopSounds()
 		end
 	elseif (iname == "Toggle") then
 		local active = value >= 1
 		if (active) then
-			self.SoundOutput:Fire("ToggleSound", "", 0)
 			self.Active = !self.Active
+		end
+		if (self.Active) then
+			self:StartSounds()
+			self.SND:ChangeVolume(self.Volume)
+			self.SND:ChangePitch(self.Pitch)
+		else
+			self:StopSounds()
 		end
 	elseif (iname == "Volume") then
 		local volume = math.Clamp(math.floor(value*10),0,10)
 		self.Volume = volume
 
-		self.SoundOutput:SetKeyValue("health",volume)
-		if (self.Active) then
-			self.SoundOutput:Fire("Volume", volume, 0)
-		end
+		self.SND:ChangeVolume(volume)
 	elseif (iname == "Play") then
 		local active = value >= 1
 		if (active) then
 			self.Active = true
 			self:StartSounds()
+			self.SND:ChangeVolume(self.Volume)
+			self.SND:ChangePitch(self.Pitch)
 		end
 	elseif (iname == "Stop") then
 		local active = value >= 1
@@ -107,30 +125,12 @@ function ENT:TriggerInput(iname, value)
 			self.Active = false
 			self:StopSounds()
 		end
-	elseif (iname == "PitchNote") then
 	elseif (iname == "PitchRelative") then
 		local relpitch = math.Clamp(math.floor(value*100),0,255)
-		self.SoundOutput:SetKeyValue("pitch",relpitch)
 		if (self.Active) then
-			self.SoundOutput:Fire("Pitch", relpitch, 0)
+			self.SND:ChangePitch(relpitch)
 		end
 		self.Pitch = relpitch
-	elseif (iname == "PitchStart") then
-		local relpitch = math.Clamp(math.floor(value*100),0,255)
-		self.SoundOutput:SetKeyValue("pitchstart",relpitch)
-	elseif (iname == "SpinUpTime") then
-		local val = math.Clamp(math.floor(value*100),0,100)
-		self.SoundOutput:SetKeyValue("spinup",val)
-	elseif (iname == "SpinDownTime") then
-		local val = math.Clamp(math.floor(value*100),0,100)
-		self.SoundOutput:SetKeyValue("spindown",val)
-	elseif (iname == "FadeInStartvolume") then
-	elseif (iname == "FadeInTime") then
-		local val = math.Clamp(math.floor(value*100),0,100)
-		self.SoundOutput:SetKeyValue("fadeinsecs",val)
-	elseif (iname == "FadeOutTime") then
-		local val = math.Clamp(math.floor(value*100),0,100)
-		self.SoundOutput:SetKeyValue("fadeoutsecs",val)
 	elseif (iname == "LFOType") then
 		local val = math.Clamp(math.floor(value),0,5)
 		self.LFOType = val
@@ -161,32 +161,29 @@ function ENT:SetSound(sound)
 	local tsound = (parsedsound or ""):gsub("[/\\]+","/")
 
 	self.SampleTable[0] = sound
-	self.SoundOutput:SetKeyValue("message", tsound)
+	self.SND = CreateSound(self.Entity, Sound(self.SampleTable[0]))
 	self:SetOverlayText( "Sound: " .. tsound )
 end
 
 function ENT:SetSample(sample)
 	if (self.SampleTable[sample]) then
 		self:StopSounds()
-		self.SoundOutput:SetKeyValue("message", self.SampleTable[sample])
 	end
 end
 
 function ENT:StartSounds()
-	self:StopSounds()
-	self.SoundOutput:Fire("Volume", self.Volume, 0) //FIXME
-	self.SoundOutput:Fire("Pitch", self.Pitch, 0)
-	self.SoundOutput:Fire("PlaySound", "", 0)
+	if (self.SND) then
+		self.SND:Play()
+	end
 end
 
 function ENT:StopSounds()
-	self.SoundOutput:Fire("Volume", "0", 0)
-	self.SoundOutput:Fire("StopSound", "", 0)
+	if (self.SND) then
+		self.SND:Stop()
+	end
 end
 
 function ENT:Think()
-	self.SoundOutput:SetPos(self.Entity:GetPos())
-
 	if (self.LFOType == 5) then //Random noise
 		if ((self.LFORate ~= 0) && (CurTime() - self.LFONoiseTime > 1 / self.LFORate)) then
 			self.LFONoiseTime = CurTime()
@@ -194,8 +191,8 @@ function ENT:Think()
 			self.LFOValue = math.random()*2-1
 
 			if (self.Active) then
-				self.SoundOutput:Fire("Pitch", self.Pitch + 100*self.LFOValue*self.LFOModPitch, 0)
-				self.SoundOutput:Fire("Volume", self.Volume + 5*self.LFOValue*self.LFOModVolume, 0)
+				self.SND:ChangePitch(self.Pitch + 100*self.LFOValue*self.LFOModPitch)
+				self.SND:ChangeVolume(self.Volume + 5*self.LFOValue*self.LFOModVolume)
 			end
 		end
 	end
