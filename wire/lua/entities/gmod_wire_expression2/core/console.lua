2abe56950708e106430e72312625e36515959c52
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2008-12-16 17:50:25 +0000

    E2: concmd() added

--- wire/lua/entities/gmod_wire_expression2/core/console.lua
+++ wire/lua/entities/gmod_wire_expression2/core/console.lua
@@ -7,3 +7,9 @@
 	local rv1 = op1[1](self, op1)
 	self.player:Msg(rv1 .. "\n")
 end)*/
+
+registerFunction("concmd", "s", "", function(self, args)
+	local op1 = args[2]
+	local rv1 = op1[1](self, op1)
+	self.player:ConCommand(rv1)
+end)
