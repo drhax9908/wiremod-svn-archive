ce2fba4a3fbdee2466f6fa7f99fe3a7cf0b2f38d
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2008-12-22 10:34:54 +0000

    E2: fix for string:sub()

--- wire/lua/entities/gmod_wire_expression2/core/string.lua
+++ wire/lua/entities/gmod_wire_expression2/core/string.lua
@@ -120,7 +120,7 @@ end)
 
 registerFunction("sub", "s:nn", "s", function(self, args)
 	local op1, op2, op3 = args[2], args[3], args[4]
-	local rv1, rv2, rv3 = op1[1](self, op1), op2[1](self, op2), op3[1](self, op2)
+	local rv1, rv2, rv3 = op1[1](self, op1), op2[1](self, op2), op3[1](self, op3)
 	return rv1:sub(rv2, rv3)
 end)
 
