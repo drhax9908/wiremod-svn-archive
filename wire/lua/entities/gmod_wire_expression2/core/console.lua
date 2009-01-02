34eac0c84721f085a5543b32852b13104fe24f4a
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-01-02 12:41:24 +0000

    E2: added convar(S):S, convarnum(S):N

--- wire/lua/entities/gmod_wire_expression2/core/console.lua
+++ wire/lua/entities/gmod_wire_expression2/core/console.lua
@@ -13,3 +13,15 @@ registerFunction("concmd", "s", "", function(self, args)
 	local rv1 = op1[1](self, op1)
 	self.player:ConCommand(rv1)
 end)
+
+registerFunction("convar", "s", "s", function(self, args)
+	local op1 = args[2]
+	local rv1 = op1[1](self, op1)
+	self.player:GetInfo(rv1) 
+end)
+
+registerFunction("convarnum", "s", "n", function(self, args)
+	local op1 = args[2]
+	local rv1 = op1[1](self, op1)
+	self.player:GetInfoNum(rv1) 
+end)
