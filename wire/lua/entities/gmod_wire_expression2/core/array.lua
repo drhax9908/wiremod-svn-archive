12d0015d445f9719095b3dd35dd991e07c234533
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-01-09 23:32:28 +0000

    E2: added array-extension (thanks Erkle)

--- wire/lua/entities/gmod_wire_expression2/core/array.lua
+++ wire/lua/entities/gmod_wire_expression2/core/array.lua
@@ -1,13 +1,17 @@
-AddCSLuaFile('table.lua')
+AddCSLuaFile('array.lua')
 
 /******************************************************************************\
-  Table support
+  Array support
 \******************************************************************************/
 
-registerType("table", "t", {},
+E2_MAX_ARRAY_SIZE = 1024*1024	// 1MB
+
+/******************************************************************************/
+
+registerType("array", "r", {},
 	function(self, input)
 		local ret = {}
-		for k,v in pairs(input) do ret[k] = v end
+		for k,v in ipairs(input) do ret[k] = v end
 		return ret
 	end,
 	function(self, output) return output end
@@ -15,13 +19,13 @@ registerType("table", "t", {},
 
 /******************************************************************************/
 
-registerFunction("table", "", "t", function(self, args)
+registerFunction("array", "", "r", function(self, args)
 	return {}
 end)
 
 /******************************************************************************/
 
-registerOperator("ass", "t", "t", function(self, args)
+registerOperator("ass", "r", "r", function(self, args)
 	local op1, op2 = args[2], args[3]
 	local      rv2 = op2[1](self, op2)
 	self.vars[op1] = rv2
@@ -31,79 +35,81 @@ end)
 
 /******************************************************************************/
 
-registerOperator("is", "t", "n", function(self, args)
+registerOperator("is", "r", "n", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
-	if #rv1 != 0
-	   then return 1 else return 0 end
+	return (type(rv1) == "table") and 1 or 0
 end)
 
 /******************************************************************************/
 
-registerFunction("count", "t:", "n", function(self, args)
+registerFunction("count", "r:", "n", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
-	return #rv1
+	return table.getn(rv1)
 end)
 
 /******************************************************************************/
 
-registerFunction("clone", "t:", "t", function(self, args)
+registerFunction("clone", "r:", "r", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
 	local ret = {}
-	for k,v in pairs(rv1) do ret[k] = v end
+	for k,v in ipairs(rv1) do ret[k] = v end
 	return ret
 end)
 
 /******************************************************************************/
 
-registerFunction("number", "t:s", "n", function(self, args)
+registerFunction("number", "r:n", "n", function(self, args)
 	local op1, op2 = args[2], args[3]
 	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
-	local ret = rv1["n" .. rv2]
-	if ret then return ret end
+	local ret = rv1[rv2]
+	if ret then return tonumber(ret) end
 	return 0
 end)
 
-registerFunction("setNumber", "t:sn", "", function(self, args)
+registerFunction("setNumber", "r:nn", "", function(self, args)
 	local op1, op2, op3 = args[2], args[3], args[4]
 	local rv1, rv2, rv3 = op1[1](self, op1), op2[1](self, op2), op3[1](self, op3)
+	if(rv2 >= E2_MAX_ARRAY_SIZE) then return end
 	if rv3 == 0 then rv3 = nil end
-	rv1["n" .. rv2] = rv3
+	rv1[rv2] = rv3
 	//self.vclk[op1] = true
 end)
 
 
-registerFunction("vector", "t:s", "v", function(self, args)
+registerFunction("vector", "r:n", "v", function(self, args)
 	local op1, op2 = args[2], args[3]
 	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
-	local ret = rv1["v" .. rv2]
-	if ret then return ret end
+	local ret = rv1[rv2]
+	if type(ret) == "table" and table.getn(ret) == 3 then return ret end
 	return { 0, 0, 0 }
 end)
 
-registerFunction("setVector", "t:sv", "", function(self, args)
+registerFunction("setVector", "r:nv", "", function(self, args)
 	local op1, op2, op3 = args[2], args[3], args[4]
 	local rv1, rv2, rv3 = op1[1](self, op1), op2[1](self, op2), op3[1](self, op3)
+	if(rv2 >= E2_MAX_ARRAY_SIZE) then return end
 	if rv3[1] == 0 and rv3[2] == 0 and rv3[3] == 0 then rv3 = nil end
-	rv1["v" .. rv2] = rv3
+	rv1[rv2] = rv3
 	//self.vclk[op1] = true
 end)
 
 
-registerFunction("string", "t:s", "s", function(self, args)
+registerFunction("string", "r:n", "s", function(self, args)
 	local op1, op2 = args[2], args[3]
 	local rv1, rv2 = op1[1](self, op1), op2[1](self, op2)
-	local ret = rv1["s" .. rv2]
-	if ret then return ret end
+	local ret = rv1[rv2]
+	if ret then return tostring(ret) end
 	return ""
 end)
 
-registerFunction("setString", "t:ss", "", function(self, args)
+registerFunction("setString", "r:ns", "", function(self, args)
 	local op1, op2, op3 = args[2], args[3], args[4]
 	local rv1, rv2, rv3 = op1[1](self, op1), op2[1](self, op2), op3[1](self, op3)
+	if(rv2 >= E2_MAX_ARRAY_SIZE) then return end
 	if rv3 == "" then rv3 = nil end
-	rv1["s" .. rv2] = rv3
+	rv1[rv2] = rv3
 	//self.vclk[op1] = true
 end)
