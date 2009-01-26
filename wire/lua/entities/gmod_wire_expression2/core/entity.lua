8cd1e33db4e45d730f55fa6968e09b1f997d3eb9
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-01-26 10:37:19 +0000

    E2: fix for velL() again

--- wire/lua/entities/gmod_wire_expression2/core/entity.lua
+++ wire/lua/entities/gmod_wire_expression2/core/entity.lua
@@ -147,8 +147,8 @@ registerFunction("velL", "e:", "v", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
 	if(!validEntity(rv1)) then return {0,0,0} end
-	local angle = entity:GetAngles()
-	local vec = entity:GetVelocity()
+	local angle = rv1:GetAngles()
+	local vec = rv1:GetVelocity()
 	vec:Rotate(Angle(-angle.p,-angle.y,-angle.r))
 	return {vec.x,vec.y,vec.z}
 end)
