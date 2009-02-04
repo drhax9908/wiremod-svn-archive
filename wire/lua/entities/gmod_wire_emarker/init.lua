e7946d4e868585028c5b986ef9bf5c83113f9cbd
tomb <tomb@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-02-04 19:02:52 +0000

    [FIXED]Wired Entity Marker now duplicates correctly and should remember the linked entity correctly.

--- wire/lua/entities/gmod_wire_emarker/init.lua
+++ wire/lua/entities/gmod_wire_emarker/init.lua
@@ -24,3 +24,25 @@ function ENT:Setup(mark)
 	Wire_TriggerOutput(self.Entity, "Entity", self.mark)
 	self:SetOverlayText( "Mark - " .. self.mark:GetClass() )
 end
+
+function ENT:BuildDupeInfo()
+	local info = self.BaseClass.BuildDupeInfo(self) or {}
+	if(self.mark && self.mark:IsValid())then
+		info.Mark = self.mark:EntIndex()
+	end
+	return info
+end 
+
+function ENT:ApplyDupeInfo(ply, ent, info, GetEntByID)
+	self.BaseClass.ApplyDupeInfo(self, ply, ent, info, GetEntByID)
+	if (info.Mark) then
+		self.mark = GetEntByID(info.Mark)
+		if (!self.mark) then
+			self.mark = ents.GetByIndex(info.Mark)
+		end
+		if(self.mark) then
+			Wire_TriggerOutput(self.Entity, "Entity", self.mark)
+			self:SetOverlayText( "Mark - " .. self.mark:GetClass() )
+		end
+	end
+end 
