79214763fe2d702491df4de5b5bf7db16dbfadd6
Syranide <Syranide@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-03-01 11:03:05 +0000

    E2: added experimental concmd client-side opt-in (wire_expression2_concmd, defaults enabled, will change later)

--- wire/lua/entities/gmod_wire_expression2/core/console.lua
+++ wire/lua/entities/gmod_wire_expression2/core/console.lua
@@ -4,6 +4,15 @@ AddCSLuaFile('console.lua')
   Console support
 \******************************************************************************/
 
+if (CLIENT) then
+	CreateClientConVar("wire_expression2_concmd", 1, true, true)
+end
+
+local function validConCmd(self)
+	if(!self.player:IsValid()) then return false end
+	return self.player:GetInfoNum("wire_expression2_concmd") != 0
+end
+
 /*registerFunction("trace", "s", "", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
@@ -13,14 +22,14 @@ end)*/
 registerFunction("concmd", "s", "", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
-	if(!self.player:IsValid()) then return end
+	if(!validConCmd(self)) then return end
 	self.player:ConCommand(rv1)
 end)
 
 registerFunction("convar", "s", "s", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
-	if(!self.player:IsValid()) then return end
+	if(!validConCmd(self)) then return end
 	local ret = self.player:GetInfo(rv1) 
 	if ret == nil then return "" end
 	return ret
@@ -29,7 +38,7 @@ end)
 registerFunction("convarnum", "s", "n", function(self, args)
 	local op1 = args[2]
 	local rv1 = op1[1](self, op1)
-	if(!self.player:IsValid()) then return end
+	if(!validConCmd(self)) then return end
 	local ret = self.player:GetInfoNum(rv1)
 	if ret == nil then return 0 end
 	return ret
