dc2bcbc77240b888d28387fa51a11012ca2f8e9d
BlackPhoenix <BlackPhoenix@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-03-31 18:39:23 +0000

    [FIXED] Made a whooops in shared file (wrong file)

--- wire/lua/entities/gmod_wire_cd_ray/shared.lua
+++ wire/lua/entities/gmod_wire_cd_ray/shared.lua
@@ -11,3 +11,43 @@ ENT.Instructions	= ""
 
 ENT.Spawnable			= false
 ENT.AdminSpawnable		= false
+
+
+
+
+function ENT:SetEffect( name )
+	self.Entity:SetNetworkedString( "Effect", name )
+end
+
+function ENT:GetEffect( name )
+	return self.Entity:GetNetworkedString( "Effect" )
+end
+
+
+function ENT:SetOn( boolon )
+	self.Entity:SetNetworkedBool( "On", boolon, true )
+end
+
+function ENT:IsOn( name )
+	return self.Entity:GetNetworkedBool( "On" )
+end
+
+function ENT:SetOffset( v )
+	self.Entity:SetNetworkedVector( "Offset", v, true )
+end
+
+function ENT:GetOffset( name )
+	return self.Entity:GetNetworkedVector( "Offset" )
+end
+
+function ENT:SetBeamRange(length)
+	self.Entity:SetNetworkedFloat("BeamLength", length)
+end
+
+function ENT:GetBeamRange()
+	return self.Entity:GetNetworkedFloat("BeamLength") or 0
+end
+
+function ENT:GetBeamLength()
+	return self.Entity:GetNetworkedFloat("BeamLength") or 0
+end
