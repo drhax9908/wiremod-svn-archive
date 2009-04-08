651220d2e960afd0dc9fb6f854a0cae212e9dace
Erkle <Erkle@21c5dc87-7110-4d66-9237-d0800a75ba64>
2009-04-08 09:11:32 +0000

    [CHANGED] Split the button models into two categories.

--- wire/lua/wire/stools/io.lua
+++ wire/lua/wire/stools/io.lua
@@ -151,6 +151,7 @@ end
 
 TOOL.ClientConVar = {
 	model = "models/props_c17/clock01.mdl",
+	model_category = "button",
 	toggle = "0",
 	value_off = "0",
 	value_on = "1",
@@ -160,7 +161,15 @@ TOOL.ClientConVar = {
 
 function TOOL.BuildCPanel(panel)
 	WireToolHelpers.MakePresetControl(panel, "wire_button")
-	ModelPlug_AddToCPanel(panel, "button", "wire_button", "#Button_Model", nil, "#Button_Model", 6)
+	ModelPlug_AddToCPanel_Multi(
+		panel,
+		{	button = "Normal",
+			button_small = "Small"
+		},
+		"wire_button",
+		"#Button_Model", nil, "#Button_Model",
+		6
+	)
 	panel:CheckBox("#WireButtonTool_toggle", "wire_button_toggle")
 	panel:CheckBox("#WireButtonTool_entityout", "wire_button_entityout")
 	panel:NumSlider("#WireButtonTool_value_on", "wire_button_value_on", -10, 10, 1)
